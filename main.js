295bd573043449ad14a93a86982075cd890cecef
ritisha123 <90776666+ritisha123@users.noreply.github.com>
2022-03-25 11:25:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function add_User(){
+    player1_name = document.getElementById("player1_username").value;
+    player2_name = document.getElementById("player2_username").value;
+    window.location = "index2.html";
+}
